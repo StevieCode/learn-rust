1f4e52348df85a943cabd702e320fca48bdb2a26
Steve Feng <stevef3721@gmail.com>
2021-01-19 12:53:11 -0800

    implemented iterator approach

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -42,15 +42,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-
-    result
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
