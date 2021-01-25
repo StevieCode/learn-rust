9944ab2f15087202e53fae907d2f06906abbf813
Steve Feng <stevef3721@gmail.com>
2021-01-25 10:41:39 -0800

    basic web server

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+/Users/steve/Documents/projects/learn_rust/hello/target/debug/deps/hello: src/main.rs
+
+/Users/steve/Documents/projects/learn_rust/hello/target/debug/deps/hello.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/steve/Documents/projects/learn_rust/hello/target/debug/hello: /Users/steve/Documents/projects/learn_rust/hello/src/main.rs
